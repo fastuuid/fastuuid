ece6ae2a9a472a55cb27a9037423e1cff059bbac
Omer Katz <omer.drow@gmail.com>
2019-05-14 16:08:30 +0300

    Added benchmarks.

---
+++ tests/test_benchmarks.py
@@ -0,0 +1,46 @@
+from concurrent.futures import ThreadPoolExecutor
+from uuid import uuid4
+
+import pytest
+
+from fastuuid import uuid4 as fastuuid4, uuid4_bulk
+
+
+@pytest.mark.benchmark(group='uuidv4')
+def test_uuidv4(benchmark):
+    benchmark(uuid4)
+
+
+@pytest.mark.benchmark(group='uuidv4')
+def test_fast_uuidv4(benchmark):
+    benchmark(fastuuid4)
+
+
+@pytest.mark.benchmark(group='uuidv4 8 threads')
+def test_uuidv4_threads(benchmark):
+    @benchmark
+    def b():
+        pool = ThreadPoolExecutor(max_workers=8)
+        for _ in range(8):
+            pool.submit(lambda: [uuid4() for _ in range(1000)])
+        pool.shutdown(wait=True)
+
+
+@pytest.mark.benchmark(group='uuidv4 8 threads')
+def test_fast_uuidv4_threads(benchmark):
+        @benchmark
+        def b():
+            pool = ThreadPoolExecutor(max_workers=8)
+            for _ in range(8):
+                pool.submit(lambda: [fastuuid4() for _ in range(1000)])
+            pool.shutdown(wait=True)
+
+
+@pytest.mark.benchmark(group='uuidv4 8 threads')
+def test_fast_uuidv4_bulk_threads(benchmark):
+        @benchmark
+        def b():
+            pool = ThreadPoolExecutor(max_workers=8)
+            for _ in range(8):
+                pool.submit(uuid4_bulk, 1000)
+            pool.shutdown(wait=True)

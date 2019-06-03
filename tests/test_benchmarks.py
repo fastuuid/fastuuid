ad2b219c0402269e2a8c86d544ce692670a7d471
Omer Katz <omer.drow@gmail.com>
2019-06-03 15:18:13 +0300

    Added benchmarks for UUID v3 and v5.

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -1,9 +1,21 @@
 from concurrent.futures import ThreadPoolExecutor
-from uuid import uuid4
+from uuid import uuid3, uuid4, uuid5
 
 import pytest
+from fastuuid import uuid3 as fastuuid3
 from fastuuid import uuid4 as fastuuid4
 from fastuuid import uuid4_bulk
+from fastuuid import uuid5 as fastuuid5
+
+
+@pytest.mark.benchmark(group='uuidv3')
+def test_uuidv3(benchmark):
+    benchmark(uuid3, uuid4(), "benchmark")
+
+
+@pytest.mark.benchmark(group='uuidv3')
+def test_fast_uuidv3(benchmark):
+    benchmark(fastuuid3, fastuuid4(), b"benchmark")
 
 
 @pytest.mark.benchmark(group='uuidv4')
@@ -14,6 +26,16 @@ def test_uuidv4(benchmark):
 @pytest.mark.benchmark(group='uuidv4')
 def test_fast_uuidv4(benchmark):
     benchmark(fastuuid4)
+
+
+@pytest.mark.benchmark(group='uuidv5')
+def test_uuidv5(benchmark):
+    benchmark(uuid5, uuid4(), "benchmark")
+
+
+@pytest.mark.benchmark(group='uuidv5')
+def test_fast_uuidv5(benchmark):
+    benchmark(fastuuid5, fastuuid4(), b"benchmark")
 
 
 @pytest.mark.benchmark(group='uuidv4 8 threads')

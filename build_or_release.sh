3c8eba1c9de669f6ad5da3fe5c4d1cfc325f0796
Omer Katz <omer.drow@gmail.com>
2019-06-05 20:42:40 +0300

    Use username from environment.

--- build_or_release.sh
+++ build_or_release.sh
@@ -3,5 +3,5 @@
 if [[ -z "${TRAVIS_TAG}" ]]; then
   /opt/python/cp37-cp37m/bin/pyo3-pack build --manylinux 2010 --release -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
 else
-  /opt/python/cp37-cp37m/bin/pyo3-pack publish --manylinux 2010 -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
+  /opt/python/cp37-cp37m/bin/pyo3-pack publish --username $PYPI_USERNAME --manylinux 2010 -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
 fi

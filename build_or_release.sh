92e1d29940dcde9aa983bcf32412fb798b98a2d5
Omer Katz <omer.drow@gmail.com>
2019-06-17 13:16:14 +0300

    Specify manylinux version via env var.

--- build_or_release.sh
+++ build_or_release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [[ -z "${TRAVIS_TAG}" ]]; then
-  /opt/python/cp37-cp37m/bin/pyo3-pack build --manylinux 2010 --release -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
+  /opt/python/cp37-cp37m/bin/pyo3-pack build --manylinux $MANYLINUX_VERSION --release -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
   ls -1 target/wheels/*.whl | xargs -I%  auditwheel show %
 else
-  /opt/python/cp37-cp37m/bin/pyo3-pack publish --username $PYPI_USERNAME --manylinux 2010 -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
+  /opt/python/cp37-cp37m/bin/pyo3-pack publish --username $PYPI_USERNAME --manylinux $MANYLINUX_VERSION -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
 fi

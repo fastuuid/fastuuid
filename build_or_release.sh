458bc5e938d7a156a50ebefd90442495b5ecd96f
Omer Katz <omer.drow@gmail.com>
2019-06-17 12:46:12 +0300

    auditwheels

--- build_or_release.sh
+++ build_or_release.sh
@@ -2,6 +2,7 @@
 
 if [[ -z "${TRAVIS_TAG}" ]]; then
   /opt/python/cp37-cp37m/bin/pyo3-pack build --manylinux 2010 --release -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
+  ls -1 target/wheels/*.whl | xargs -I%  auditwheel show %
 else
   /opt/python/cp37-cp37m/bin/pyo3-pack publish --username $PYPI_USERNAME --manylinux 2010 -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
 fi

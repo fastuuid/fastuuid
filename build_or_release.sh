5e81f9a29f136fe1b5240481d64ee571076669da
Omer Katz <omer.drow@gmail.com>
2020-05-20 11:02:39 +0300

    Only publish sdist from one of the jobs.

--- build_or_release.sh
+++ build_or_release.sh
@@ -4,5 +4,5 @@ if [[ -z "${TRAVIS_TAG}" ]]; then
   /opt/python/cp38-cp38/bin/maturin build --manylinux $MANYLINUX_VERSION --release -i /opt/python/cp38-cp38/bin/python -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
   ls -1 target/wheels/*.whl | xargs -I%  auditwheel show %
 else
-  /opt/python/cp38-cp38/bin/maturin publish --username $PYPI_USERNAME --manylinux $MANYLINUX_VERSION -i /opt/python/cp38-cp38/bin/python -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
+  /opt/python/cp38-cp38/bin/maturin publish ${SDIST:---no-sdist} --username $PYPI_USERNAME --manylinux $MANYLINUX_VERSION -i /opt/python/cp38-cp38/bin/python -i /opt/python/cp37-cp37m/bin/python -i /opt/python/cp36-cp36m/bin/python -i /opt/python/cp35-cp35m/bin/python
 fi
